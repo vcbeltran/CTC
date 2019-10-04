67e0822d7bc5e89f1ac830c82afa5f6191d9b2bb
Vero <Vero@DESKTOP-C0E61DI>
2019-10-04 22:25:16 +0200

    Sigo con pruebas (commit en local)

---
+++ pruebafuncionaporfavortelopido.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of pruebafuncionaporfavortelopido
+ *
+ * @author Vero
+ */
+class pruebafuncionaporfavortelopido {
+    
+    public function pinta(){
+        echo "joputa";
+        echo "joputa";
+    }
+    
+}
