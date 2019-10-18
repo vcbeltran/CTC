bea6bb517cdd802c389a46c1abfe3a6a19de8571
Vero <Vero@DESKTOP-C0E61DI>
2019-10-19 01:43:36 +0200

    Agregando modificar empresas (OK)

--- actualizaEmpresa.php
+++ actualizaEmpresa.php
@@ -6,8 +6,12 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+          <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <title>Actualiza usuario empresa</title>
     </head>
     <body>
         <?php
@@ -25,11 +29,26 @@ and open the template in the editor.
         var_dump($correo);
         var_dump($contra);
         var_dump($local);
+
         if ($consulta->actualizarEmpresa($codigo, $nombre, $correo, $contra, $local)) {
-            echo "ha ido bien";
-        } else {
-            echo "ha ido como el culito";
-        }
-        ?>
+            ?>
+            <div class="container mt-5" >
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+                    <div class="alert alert-success">
+                        <strong>¡Ya está actualizada la empresa!</strong> Puedes volver a la página de administrador <a href="menuAdministrador.php" class="alert-link">pulsando aquí</a>.
+                    </div> 
+                </div>
+            </div>
+        <?php } else { ?>
+            <div class="container  mt-5" >
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+                    <div class="alert alert-danger">
+                        <strong>¡Lo sentimos!</strong> El nombre de la empresa ya está en uso.<a href="formularioAltaEmpresa.php" class="alert-link"> Pulse para volver atrás</a>
+                    </div>  
+                </div>
+            </div>    
+        <?php } ?>
     </body>
 </html>
