8d6289bda6dece0b55797bad5b83e93c7af66270
52956653 <52956653@i5703.dimserso.int>
2019-10-31 12:56:05 +0100

    Cambiando los link de la cabecera en un archivo externo en el perfil USUARIO ONLINE

--- loginUsuario.php
+++ loginUsuario.php
@@ -7,10 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <?php include ('ncludes/include.php'); ?>
         <title>Bienvenido</title>
     </head>
     <body>
