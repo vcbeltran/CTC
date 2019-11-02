4398cb0d28880d17ce6f171f443ed4fc310a8051
Vero <Vero@DESKTOP-C0E61DI>
2019-11-02 14:48:50 +0100

    Formalizando datos de reserva (haciendo insert)

---
+++ consultas/consultasReservas.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of consultasReservas
+ *
+ * @author Vero
+ */
+class consultasReservas {
+    //put your code here
+    private $conexion;
+    public function __construct() {
+        $this->conexion = new mysqli('localhost', 'vero', 'proyectodaw', 'ctc');
+        if ($this->conexion->connect_error) {
+            die("Error de conexión(" . $this->conexion->connect_errno . ") "
+                    . $this->conexion->connect_errno);
+        }
+    } 
+    
+    public function insertarReserva($fechaRealiza, $idCodigoFecha, $idUsuario){
+        $alta = "INSERT INTO RESERVA (FECHAREALIZA, IDLOCALFECHAPRECIO, IDUSUARIO) "
+                . " VALUES ('$fechaRealiza', '$idCodigoFecha', '$idUsuario')";
+        echo $alta;
+    if ($this->conexion->query($alta)) {
+            return true;
+        }
+    }
+}
