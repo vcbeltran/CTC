37f0e6205e335406f8f9f728ea6b3f6d8905aa2c
52956653 <52956653@i5703.dimserso.int>
2019-11-05 10:16:59 +0100

    Añadiendo pantalla error login Redirección en admin

---
+++ reloguin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <?php include ('includes/include.php'); ?>
+        <title>Vuelva a loguearse</title>
+    </head>
+    <body>
+        <?php
+        // clase para dar aviso de error
+        ?>
+        <div class="container mt-5">
+            <div class="alert alert-danger" role="alert">
+                <h4 class="alert-heading"><i class="fas fa-exclamation-triangle"></i> ¡Lo sentimos!</h4>
+                <p> Algún dato introducido no es correcto.</p>
+                <hr>
+                <p class="mb-0"> <a class="alert-link" href="formularioLogin.php"><strong>Pulse aquí para volver al formulario introducción de datos.</strong></a></p>
+            </div>
+        </div>
+    </body>
+</html>
